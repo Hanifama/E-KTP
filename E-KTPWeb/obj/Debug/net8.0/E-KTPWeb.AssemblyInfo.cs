c393b29e13bb6813ed63159420f401d031138a4e
hanifama <hanifamafisial.com>
2024-05-08 11:04:59 +0700

    fix bug

--- E-KTPWeb/obj/Debug/net8.0/E-KTPWeb.AssemblyInfo.cs
+++ E-KTPWeb/obj/Debug/net8.0/E-KTPWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-KTPWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d7038a5fbaa13f5d0be995058699791e623592")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-KTPWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-KTPWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
